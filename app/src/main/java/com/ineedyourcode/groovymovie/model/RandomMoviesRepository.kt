c5620e4e12f3c7acb2d19af0bade83d708869987
Dmitry Gusev <dmtrxiii@gmail.com>
2022-01-16 22:18:04 +0300

    recycler views by genres released

--- app/src/main/java/com/ineedyourcode/groovymovie/model/RandomMoviesRepository.kt
+++ app/src/main/java/com/ineedyourcode/groovymovie/model/RandomMoviesRepository.kt
@@ -6,19 +6,41 @@ import kotlin.random.Random
 class RandomMoviesRepository : IMoviesRepository {
 
     private val moviesList = mutableListOf<Movie>()
+    private val moviesComedyList = mutableListOf<Movie>()
+    private val moviesDramaList = mutableListOf<Movie>()
+    private val moviesActionList = mutableListOf<Movie>()
+    private val moviesFamilyList = mutableListOf<Movie>()
+    private val moviesTrillerList = mutableListOf<Movie>()
 
     init {
-        (0..19).forEach { i ->
+        (0..99).forEach { i ->
             moviesList.add(
                 Movie(
-                    "Movie_$i" + "_название фильма/сериала",
-                    (2000 + i).toString(),
-                    (Random.nextDouble(1.0, 5.0)).toString().substring(0, 3),
-                    "Какой-то жанр",
+                    "Movie_$i" + "_название фильма",
+                    (1990..2022).random().toString(),
+                    (Random.nextDouble(1.0, 5.0)).toString().substring(0, 3), randomGenre(),
                     R.drawable.tmdb_logo
                 )
             )
+            when(moviesList[i].genre) {
+                "Комедия" -> moviesComedyList.add(moviesList[i])
+                "Боевик" -> moviesActionList.add(moviesList[i])
+                "Триллер" -> moviesTrillerList.add(moviesList[i])
+                "Драма" -> moviesDramaList.add(moviesList[i])
+                "Семейный" -> moviesFamilyList.add(moviesList[i])
+            }
         }
+    }
+
+    private fun randomGenre(): String {
+        when((1..5).random()) {
+            1 -> return "Комедия"
+            2 -> return "Боевик"
+            3 -> return "Триллер"
+            4 -> return "Драма"
+            5 -> return "Семейный"
+        }
+        return ""
     }
 
     override fun getMoviesList(): List<Movie> {
--- app/src/main/java/com/ineedyourcode/groovymovie/view/MainScreenFragment.kt
+++ app/src/main/java/com/ineedyourcode/groovymovie/view/MainScreenFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.ProgressBar
+import android.widget.ScrollView
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -24,10 +25,27 @@ import com.ineedyourcode.groovymovie.viewmodel.MainScreenViewModel
 
 class MainScreenFragment : Fragment() {
 
-    private lateinit var adapter: MovieListAdapter
+    private lateinit var adapterComedy: MovieListAdapter
+    private lateinit var adapterDrama: MovieListAdapter
+    private lateinit var adapterTriller: MovieListAdapter
+    private lateinit var adapterFamily: MovieListAdapter
+    private lateinit var adapterAction: MovieListAdapter
+
     private lateinit var moviesList: List<Movie>
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var moviesListComedy: List<Movie>
+    private lateinit var moviesListDrama: List<Movie>
+    private lateinit var moviesListTriller: List<Movie>
+    private lateinit var moviesListFamily: List<Movie>
+    private lateinit var moviesListAction: List<Movie>
+
+    private lateinit var recyclerViewAction: RecyclerView
+    private lateinit var recyclerViewComedy: RecyclerView
+    private lateinit var recyclerViewDrama: RecyclerView
+    private lateinit var recyclerViewTriller: RecyclerView
+    private lateinit var recyclerViewFamily: RecyclerView
+
     private lateinit var progressCircular: ProgressBar
+    private lateinit var scrollView: ScrollView
     private var _binding: FragmentMainScreenBinding? = null
     private val binding get() = _binding!!
 
@@ -49,23 +67,67 @@ class MainScreenFragment : Fragment() {
             renderData(it as AppState)
         })
 
-        adapter = MovieListAdapter()
+        adapterComedy = MovieListAdapter()
+        adapterDrama = MovieListAdapter()
+        adapterTriller = MovieListAdapter()
+        adapterFamily = MovieListAdapter()
+        adapterAction = MovieListAdapter()
 
         val searchLayout = binding.tfInputSearch
         val searchValue = binding.tfEditSearch
         progressCircular = binding.progressCircular
+        scrollView = binding.scroll
 
-        recyclerView = binding.moviesRecyclerview
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = adapter
-        adapter.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
+        recyclerViewAction = binding.moviesRecyclerviewAction
+        recyclerViewComedy = binding.moviesRecyclerviewComedy
+        recyclerViewDrama = binding.moviesRecyclerviewDrama
+        recyclerViewTriller = binding.moviesRecyclerviewTriller
+        recyclerViewFamily = binding.moviesRecyclerviewFamily
+
+        recyclerViewAction.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewComedy.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewDrama.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewTriller.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewFamily.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        recyclerViewAction.adapter = adapterAction
+        recyclerViewComedy.adapter = adapterComedy
+        recyclerViewDrama.adapter = adapterDrama
+        recyclerViewTriller.adapter = adapterTriller
+        recyclerViewFamily.adapter = adapterFamily
+
+        adapterAction.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
             override fun onItemClickListener(position: Int) {
-                val selectedMovie = moviesList[position]
-                parentFragmentManager.beginTransaction()
-                    .setTransition(TRANSIT_FRAGMENT_OPEN)
-                    .replace(R.id.fragment_container, MovieInfoFragment.newInstance(selectedMovie))
-                    .addToBackStack("")
-                    .commit()
+                onMovieClick(moviesListAction, position)
+            }
+        })
+
+        adapterComedy.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
+            override fun onItemClickListener(position: Int) {
+                onMovieClick(moviesListComedy, position)
+            }
+        })
+
+        adapterDrama.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
+            override fun onItemClickListener(position: Int) {
+                onMovieClick(moviesListDrama, position)
+            }
+        })
+
+        adapterTriller.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
+            override fun onItemClickListener(position: Int) {
+                onMovieClick(moviesListTriller, position)
+            }
+        })
+
+        adapterFamily.setOnItemClickListener(object : MovieListAdapter.OnItemClickListener {
+            override fun onItemClickListener(position: Int) {
+                onMovieClick(moviesListFamily, position)
             }
         })
 
@@ -95,23 +157,48 @@ class MainScreenFragment : Fragment() {
         when (appState) {
             is AppState.Success -> {
                 progressCircular.isVisible = false
-                recyclerView.isVisible = true
+                scrollView.visibility = View.VISIBLE
+
                 moviesList = appState.moviesData
-                adapter.setAdapterData(moviesList)
+                filterByGenres(moviesList)
+
             }
             is AppState.Loading -> {
                 progressCircular.isVisible = true
-                recyclerView.visibility = View.INVISIBLE
+                scrollView.visibility = View.INVISIBLE
             }
             is AppState.Error -> {
                 progressCircular.isVisible = false
-                recyclerView.visibility = View.INVISIBLE
+                scrollView.visibility = View.INVISIBLE
                 Snackbar
                     .make(binding.tfInputSearch, appState.error, Snackbar.LENGTH_INDEFINITE)
                     .setAction(getString(R.string.retry)) { viewModel.getData() }
                     .show()
             }
         }
+    }
+
+    private fun filterByGenres(moviesList: List<Movie>) {
+        moviesListComedy = moviesList.filter { it.genre == "Комедия" }
+        moviesListAction = moviesList.filter { it.genre == "Боевик" }
+        moviesListTriller = moviesList.filter { it.genre == "Триллер" }
+        moviesListDrama = moviesList.filter { it.genre == "Драма" }
+        moviesListFamily = moviesList.filter { it.genre == "Семейный" }
+
+        adapterAction.setAdapterData(moviesListAction)
+        adapterComedy.setAdapterData(moviesListComedy)
+        adapterDrama.setAdapterData(moviesListDrama)
+        adapterTriller.setAdapterData(moviesListTriller)
+        adapterFamily.setAdapterData(moviesListFamily)
+    }
+
+    private fun onMovieClick(moviesList: List<Movie>, position: Int) {
+        val selectedMovie = moviesList[position]
+        parentFragmentManager.beginTransaction()
+            .setTransition(TRANSIT_FRAGMENT_OPEN)
+            .replace(R.id.fragment_container, MovieInfoFragment.newInstance(selectedMovie))
+            .addToBackStack("")
+            .commit()
     }
 
     override fun onDestroy() {
