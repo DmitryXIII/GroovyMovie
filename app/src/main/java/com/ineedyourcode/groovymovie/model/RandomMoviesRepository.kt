2b1753ff47ec9660cf130d3853baef187347bd0e
Dmitry Gusev <dmtrxiii@gmail.com>
2022-01-20 11:49:58 +0300

    some small changes

--- app/src/main/java/com/ineedyourcode/groovymovie/model/RandomMoviesRepository.kt
+++ app/src/main/java/com/ineedyourcode/groovymovie/model/RandomMoviesRepository.kt
@@ -6,11 +6,6 @@ import kotlin.random.Random
 class RandomMoviesRepository : IMoviesRepository {
 
     private val moviesList = mutableListOf<Movie>()
-    private val moviesComedyList = mutableListOf<Movie>()
-    private val moviesDramaList = mutableListOf<Movie>()
-    private val moviesActionList = mutableListOf<Movie>()
-    private val moviesFamilyList = mutableListOf<Movie>()
-    private val moviesTrillerList = mutableListOf<Movie>()
 
     init {
         (0..99).forEach { i ->
@@ -18,22 +13,16 @@ class RandomMoviesRepository : IMoviesRepository {
                 Movie(
                     "Movie_$i" + "_название фильма",
                     (1990..2022).random().toString(),
-                    (Random.nextDouble(1.0, 5.0)).toString().substring(0, 3), randomGenre(),
+                    (Random.nextDouble(1.0, 5.0)).toString().substring(0, 3),
+                    randomGenre(),
                     R.drawable.tmdb_logo
                 )
             )
-            when(moviesList[i].genre) {
-                "Комедия" -> moviesComedyList.add(moviesList[i])
-                "Боевик" -> moviesActionList.add(moviesList[i])
-                "Триллер" -> moviesTrillerList.add(moviesList[i])
-                "Драма" -> moviesDramaList.add(moviesList[i])
-                "Семейный" -> moviesFamilyList.add(moviesList[i])
-            }
         }
     }
 
     private fun randomGenre(): String {
-        when((1..5).random()) {
+        when ((1..5).random()) {
             1 -> return "Комедия"
             2 -> return "Боевик"
             3 -> return "Триллер"
