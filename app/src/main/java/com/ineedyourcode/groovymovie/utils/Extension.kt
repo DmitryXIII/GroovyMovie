979c8f6e30ba50329d7d075a1196863538cc944a
Dmitry Gusev <dmtrxiii@gmail.com>
2022-01-28 14:17:05 +0300

    on internet connection changed action listener released

--- app/src/main/java/com/ineedyourcode/groovymovie/utils/Extension.kt
+++ app/src/main/java/com/ineedyourcode/groovymovie/utils/Extension.kt
@@ -15,7 +15,7 @@ fun View.hideKeyboard(): Boolean {
 //снэкбар без action, принимает в параметрах строковый ресурс
 fun View.showSnackWithoutAction(@StringRes message: Int) {
     Snackbar.make(this, message, Snackbar.LENGTH_SHORT).apply {
-        setBackgroundTint(resources.getColor(R.color.appbar_layuot, context.theme))
+        setBackgroundTint(resources.getColor(R.color.appbar_layout, context.theme))
         setTextColor(resources.getColor(R.color.white, context.theme))
         show()
     }
@@ -24,7 +24,7 @@ fun View.showSnackWithoutAction(@StringRes message: Int) {
 //снэкбар без action, принимает в параметрах String
 fun View.showSnackWithoutAction(message: String) {
     Snackbar.make(this, message, Snackbar.LENGTH_SHORT).apply {
-        setBackgroundTint(resources.getColor(R.color.appbar_layuot, context.theme))
+        setBackgroundTint(resources.getColor(R.color.appbar_layout, context.theme))
         setTextColor(resources.getColor(R.color.white, context.theme))
         show()
     }
@@ -32,7 +32,7 @@ fun View.showSnackWithoutAction(message: String) {
 
 fun View.showSnackWithAction(message: String, actionText: String, action: (View) -> Unit) {
     Snackbar.make(this, message, Snackbar.LENGTH_INDEFINITE).setAction(actionText, action).apply {
-        setBackgroundTint(resources.getColor(R.color.appbar_layuot, context.theme))
+        setBackgroundTint(resources.getColor(R.color.appbar_layout, context.theme))
         setTextColor(resources.getColor(R.color.white, context.theme))
         setActionTextColor(resources.getColor(R.color.orange, context.theme))
         show()
--- app/src/main/java/com/ineedyourcode/groovymovie/view/MainActivity.kt
+++ app/src/main/java/com/ineedyourcode/groovymovie/view/MainActivity.kt
@@ -1,18 +1,53 @@
 package com.ineedyourcode.groovymovie.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN
+import androidx.lifecycle.Observer
+import com.ineedyourcode.groovymovie.utils.ConnectivityLiveData
 import com.ineedyourcode.groovymovie.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var isConnected = true
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager
             .beginTransaction()
+            .setTransition(TRANSIT_FRAGMENT_OPEN)
             .replace(R.id.fragment_container, MainScreenFragment())
             .commit()
+
+        ConnectivityLiveData(this)
+            .observe(this, Observer { isConnectFromReceiver ->
+                if (!isConnectFromReceiver && isConnected) {
+                    isConnected = false
+                    supportFragmentManager
+                        .beginTransaction()
+                        .setTransition(TRANSIT_FRAGMENT_FADE)
+                        .replace(
+                            R.id.fragment_no_connection_container,
+                            ConnectivityFragment(),
+                            "NO_CONNECTION_FRAGMENT"
+                        )
+                        .commit()
+                } else if (isConnectFromReceiver && isConnected == false) {
+                    isConnected = true
+                    supportFragmentManager
+                        .findFragmentByTag("NO_CONNECTION_FRAGMENT")?.let { fragment ->
+                            supportFragmentManager
+                                .beginTransaction()
+                                .remove(fragment)
+                                .commit()
+                        }
+                }
+            })
     }
 }
